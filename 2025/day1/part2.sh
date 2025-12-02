521b9e8c3a77d4b2dc84a52541a31fc4cb6652b3
Zach Barge <zachb.barge@gmail.com>
2025-12-02 16:15:06 -0500

    feat(aoc2025-day1): Continued effort to solve day 2. Got closer to the correct answer and example yields correct answer

--- 2025/day1/part2.sh
+++ 2025/day1/part2.sh
@@ -4,6 +4,8 @@ dial=50
 
 zero_count=0
 
+echo "starting at $dial"
+
 while read -r instruction; do
 	dir="${instruction:0:1}"
 
@@ -15,25 +17,29 @@ while read -r instruction; do
 
 	echo "moving $dir by $amount"
 
+	prev_dial=$dial
+
 	if [[ $dir == 'L' ]]; then
-		dial_start=$dial
-		((dial=((dial-amount) + 100) % 100))
+		new_dial=$((dial-amount))
+		((dial=((new_dial) + 100) % 100))
 
 		# we went around the dial at least once
-		if (( dial_start < dial )); then
+		if (( new_dial <= 0 && prev_dial != 0 )); then
 			echo "went around dial $scale_factor times"
 			((zero_count+=scale_factor))
 		fi
 
 	elif [[ $dir == 'R' ]]; then
-		dial_start=$dial
-		((dial=((dial + amount) + 100) % 100))
+		new_dial=$((dial+amount))
+		((dial=((new_dial) + 100) % 100))
 		# we went around the dial at least once
-		if (( dial_start > dial )); then
+		if (( new_dial >= 99 )); then
 			echo "went around dial $scale_factor times"
 			((zero_count+=scale_factor))
 		fi
 	fi
+
+	echo "Dial is now at $dial"
 done
 
 echo $zero_count
